fd92d48bab077f260cab1a5d096bf69226639b48
arthurbaptista <arthurdbaptista@hotmail.com>
2025-11-06 12:43:35 -0300

    subindo alteracoes.

--- backend/ms-autenticacao/src/main/java/com/dac/autenticacao/config/SecurityConfig.java
+++ backend/ms-autenticacao/src/main/java/com/dac/autenticacao/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean // <-- Este é o Bean que estava faltando
+    @Bean //
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
