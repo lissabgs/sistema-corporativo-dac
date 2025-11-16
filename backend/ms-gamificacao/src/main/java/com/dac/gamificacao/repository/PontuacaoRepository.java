c47d6f209cd9a7b04e95ce81f337acfc3a382620
arthurbaptista <arthurdbaptista@hotmail.com>
2025-11-15 23:54:37 -0300

    ADICIONANDO MS de cursos, progresso e gamificação.

--- backend/ms-gamificacao/src/main/java/com/dac/gamificacao/repository/PontuacaoRepository.java
+++ backend/ms-gamificacao/src/main/java/com/dac/gamificacao/repository/PontuacaoRepository.java
@@ -1,3 +1,5 @@
+package com.dac.gamificacao.repository;
+
 import com.dac.gamificacao.model.Pontuacao;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
@@ -7,4 +9,3 @@ public interface PontuacaoRepository extends MongoRepository<Pontuacao, String> 
     // Busca um documento de pontuação pelo ID do funcionário
     Optional<Pontuacao> findByFuncionarioId(Long funcionarioId);
 }
-
